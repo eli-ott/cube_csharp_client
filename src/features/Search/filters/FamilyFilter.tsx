54cbd190e1f350fff8740def661a70ac5ffa1f78
Gosselin Noa <no.gosselin2006@gmail.com>
2025-03-15 23:19:23 +0100

    :bug: fix: reset selected family and search term when no family is selected

--- src/features/Search/filters/FamilyFilter.tsx
+++ src/features/Search/filters/FamilyFilter.tsx
@@ -37,6 +37,9 @@ const FamilyFilter = () => {
         setSelectedFamily(family);
         setSearchTerm(family.name);
       }
+    } else {
+      setSelectedFamily(null);
+      setSearchTerm("");
     }
   }, [families, searchParams]);
 
